b50048e8bfd94439de3a60bc4f2062f5f9ddec29
unknown <ameliaL2604@gmail.com>
2024-04-21 14:09:10 +0700

    Tugas Regex

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./tailwind-components/*.{html.js}","./regular-expression/*.{html,js}"],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
